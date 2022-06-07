a0c8fecfc37730ea2ae5206df7fd6411def3dcb9
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2022-06-07 17:29:28 +0300

    feat: подготовить структуру

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
