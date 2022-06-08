e799cf5b9f66b39b9194dda9d2801f9ecebe94b3
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2022-06-08 14:06:43 +0300

    feat: добавить адаптив до 375px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,46 @@
 import "./Header.css";
+import { useState, useEffect } from "react";
 import SearchForm from "../SearchForm/SearchForm";
 import logo from "../../images/header_logo.svg";
-import avatar from "../../images/avatar.png";
+import userAvatar from "../../images/avatar.png";
+import profilePic from "../../images/profile_pic.svg";
 
 export default function Header() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () =>
+      setTimeout(() => {
+        handleResize();
+      }, 500)
+    );
+  }, []);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
   return (
-    <header className="header">
-      <img src={logo} alt="Логотип" className="header__logo" />
-      <SearchForm />
-      <div className="header__profile">
-        <p className="header__profile-name">Константин</p>
-        <img src={avatar} alt="Аватар" className="header__profile-avatar" />
-      </div>
-    </header>
+    <>
+      {screenWidth > 518 ? (
+        <header className="header">
+          <img src={logo} alt="Логотип" className="header__logo" />
+          <SearchForm />
+          <div className="header__profile">
+            <p className="header__profile-name">Константин</p>
+            <img src={userAvatar} alt="Аватар" className="header__profile-avatar" />
+          </div>
+        </header>
+      ) : (
+        <header className="header">
+          <img src={logo} alt="Логотип" className="header__logo" />
+          <div className="header__profile">
+            <img src={profilePic} alt="Аватар" className="header__profile-avatar" />
+          </div>
+          <SearchForm />
+        </header>
+        
+      )}
+    </>
   );
 }
